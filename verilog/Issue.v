5a8014e48ae71b508ddd114c6cbf91d3957adaeb
Xenon000 <zchen61@u.rochester.edu>
2019-12-12 14:17:29 -0500

    issue queue ready bit mechanism to be debugged

--- verilog/Issue.v
+++ verilog/Issue.v
@@ -16,13 +16,10 @@ module Issue (
     input [31:0] exe_broadcast_val,
 
     //to execution
-
     output reg [5:0]    RegWr_exe,    
     output reg [31:0]   instr_exe,
     output reg [31:0]   instru_pc_exe,
 
-    output reg [31:0]   operandA1_exe,
-    output reg [31:0]   operandB1_exe,
     output reg [4:0]    shamt_exe,
     output reg [5:0]    ALU_con_exe,
     output reg          RegWr_exe,
@@ -33,7 +30,8 @@ module Issue (
     output reg          jumpReg_exe,
     output reg [31:0]   MemWriteData_exe,
 
-
+    output reg [31:0]   operandA1_exe,
+    output reg [31:0]   operandB1_exe,
 
     
     //ROB
@@ -44,6 +42,7 @@ module Issue (
     //do branch in execution
 );
 reg [193:0] issue_q [15:0];
+reg [31:0] Operand_q [1:0][15:0];
 reg [15:0] ready_q [1:0];
 reg [15:0] empty_in_issue;
 
@@ -53,23 +52,25 @@ wire [5:0] MapB     =  rename_issueinfo[11:6];
 wire [5:0] MapWr    =  rename_issueinfo[17:12];
 wire [31:0] Instr   = rename_issueinfo[49:18];
 wire [31:0] Instr_pc = rename_issueinfo[81:50];
-reg  [31:0] OpA1;
-wire [31:0] OpB1    = rename_issueinfo[113:82];
-wire [4:0] shamt    = rename_issueinfo[118:114];
-wire [5:0] alu_con  = rename_issueinfo[124:119];
-wire RegWr_flag     = rename_issueinfo[125];
-wire MemWr_flag     = rename_issueinfo[126];
-wire MemRd_flag     = rename_issueinfo[127];
-wire branch_flag    = rename_issueinfo[128];
-wire jump_flag      = rename_issueinfo[129];
-reg  [31:0]MemWriteData;
+wire [4:0] shamt    = rename_issueinfo[86:82];
+wire [5:0] alu_con  = rename_issueinfo[92:87];
+wire RegWr_flag     = rename_issueinfo[93];
+wire MemWr_flag     = rename_issueinfo[94];
+wire MemRd_flag     = rename_issueinfo[95];
+wire branch_flag    = rename_issueinfo[96];
+wire jump_flag      = rename_issueinfo[97];
 
-wire RegDest_flag   = rename_issueinfo[130];
-wire link_flag      = rename_issueinfo[131];
-wire [1:0] hilo     = rename_issueinfo[133:132];
-wire sys_flag       = rename_issueinfo[134];
-wire jr_flag        = rename_issueinfo[135];
-wire ALUSrc_flag    = rename_issueinfo[136];
+wire RegDest_flag   = rename_issueinfo[98];
+wire link_flag      = rename_issueinfo[99];
+wire [1:0] hilo     = rename_issueinfo[101:100];
+wire sys_flag       = rename_issueinfo[102];
+wire jr_flag        = rename_issueinfo[103];
+wire ALUSrc_flag    = rename_issueinfo[104];
+
+wire [31:0] OpA1    = rename_issueinfo[136:105];
+wire [31:0] OpB1    = rename_issueinfo[168:137];
+
+reg  [31:0]MemWriteData;
 
 integer instr_out_index;
 integer empty_spot;
@@ -96,25 +97,39 @@ initial begin
     empty_in_issue = 16'b1111111111111111;
 end
 
-//matching exe broadcast reg with current reg
-    input exe_broadcast;
-    input [5:0] exe_broadcast_map,
-    input [31:0] exe_broadcast_val,
+
+//updating ready queue and operand queue
 always @(exe_broadcast or FLUSH or enque) begin
     if (FLUSH) begin
         i = 0;
         ready_q[0] = 0;
         ready_q[1] = 0;
-        // for (i = 0; i < 16; i = i+1)begin
-        //     issue_q[i] = 0;
-        // end
-    end else if (!STALL and enque)begin
+        for (i = 0; i < 16; i = i+1)begin
+            Operand_q[0][i] = 0;
+            Operand_q[1][i] = 0;
+        end
+    end else if (!STALL and enque and (empty_spot < 16))begin
         ready_q[0][empty_spot] = (MapA == 0) ? 1 : busy[MapA];//(jump_flag & jr_flag) ? 1 : (link_flag ? 0 :1);
         ready_q[1][empty_spot] = (MapB == 0) ? 1 : busy[MapB];
-    end else if (!STALL and exe_broadcast) begin
+        Operand_q[0][empty_spot] = (MapA == 0) ? OpA1 : 0;
+        Operand_q[1][empty_spot] = (MapB == 0) ? OpB1 : 0;
+    end 
+
+    //matching exe broadcast reg with current reg
+    if (!FLUSH and !STALL and exe_broadcast) begin
         for (i = 0; i < 16; i = i+1)begin
-            ready_q[0][i] = (issue_q[i][5:0] == exe_broadcast_map) ? 1 : ready_q[0][i];
-            ready_q[1][i] = (issue_q[i][11:6] == exe_broadcast_map) ? 1 : ready_q[1][i];
+            if (empty_in_issue[i] != 1) begin
+                Operand_q[0][i] = ((issue_q[i][5:0] == exe_broadcast_map) & (issue_q[i][5:0] != 0)) ? exe_broadcast_val : Operand_q[0][i];
+                Operand_q[1][i] = ((issue_q[i][11:6] == exe_broadcast_map) & (issue_q[i][11:6] != 0)) ? exe_broadcast_val : Operand_q[1][i];         
+                ready_q[0][i] = (issue_q[i][5:0] == exe_broadcast_map) ? 1 : ready_q[0][i];
+                ready_q[1][i] = (issue_q[i][11:6] == exe_broadcast_map) ? 1 : ready_q[1][i];
+            end else begin
+            //we actually don't need this since we can check if item is not valid in issue queue with the empty_in_issue array, but just in case. 
+                Operand_q[0][i] = 0;
+                Operand_q[1][i] = 0;         
+                ready_q[0][i] = 0;
+                ready_q[1][i] = 0;
+            end
         end
     end
 
